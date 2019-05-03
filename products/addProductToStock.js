c964068514a9642d04b7ba1c9c9c5b4e3aee2a98
kaisukez <zykigix@hotmail.com>
2019-05-03 16:04:20 +0700

    add file

---
+++ products/addProductToStock.js
@@ -0,0 +1,15 @@
+module.exports.addProductToStock = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'add product to stock is not finished, feel free to write some code',
+      // input: event,
+    }),
+  }
+
+  return response
+  // callback(null, response)
+}
