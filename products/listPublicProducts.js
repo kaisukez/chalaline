de355224a67844afad501f47d9b57cac0427aa71
kaisukez <zykigix@hotmail.com>
2019-05-03 19:47:47 +0700

    list public products

--- products/listPublicProducts.js
+++ products/listPublicProducts.js
@@ -1,12 +1,56 @@
+const { scan } = require('../helpers/index')
+
+const filterOnlyPublicProduct = stores => {
+  const newStores = stores
+    .map(store => {
+      store.stocks = store.stocks.filter(product => product.isPublic === true)
+      return store
+    })
+    .filter(store => store.stocks.length !== 0)
+  return newStores
+}
+
+// const changeDataStructure = filtered => {
+//   return filtered.map(store => {
+//     store.stocks = store.stocks.map(product => ({
+//       ...product,
+//       storeID: store.storeID,
+//       storeName: store.storeName,
+//       branchName: store.branchName
+//     }))
+//     return store.stocks
+//   })
+// }
+
 module.exports.listPublicProducts = async (event, context, callback) => {
+  const TableName = process.env.STORE_TABLE
+
+  const { storeName } = JSON.parse(event.body)
+
+  const params = {
+    TableName,
+    FilterExpression: '#storeName <> :storeName',
+    ExpressionAttributeNames: {
+      '#storeName': 'storeName',
+    },
+    ExpressionAttributeValues: {
+      ':storeName': storeName,
+    },
+    ProjectionExpression: 'storeID, storeName, branchName, stocks'
+  }
+
+  const result = await scan(params)
+
+  const stores = result.Items
+  const filtered = filterOnlyPublicProduct(stores)
+
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify({
-      message: 'list public products is not finished, feel free to write some code',
-      // input: event,
+      publicProducts: filtered
     }),
   }
 
