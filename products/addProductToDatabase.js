972c597442173cd786e8b963a800965ca1e3d330
kaisukez <zykigix@hotmail.com>
2019-05-03 16:11:24 +0700

    list product in database and stock

--- products/addProductToDatabase.js
+++ products/addProductToDatabase.js
@@ -1,12 +1,35 @@
+const { put } = require('../helpers/index')
+
 module.exports.addProductToDatabase = (event, context, callback) => {
+  const TableName = process.env.PRODUCT_TABLE
+
+  const { productID, productName, ...optional } = JSON.parse(event.body)
+
+  const params = {
+    TableName,
+    Item: {
+      productID,
+      productName,
+      ...optional
+    }
+  }
+
+  let message
+
+  try {
+    const result = await put(params)
+    message = `[${productID}] ${productName} was added to database.`
+  } catch {
+    message = `some error occur`
+  }
+
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify({
-      message: 'add product to database is not finished, feel free to write some code',
-      // input: event,
+      message
     }),
   }
 
