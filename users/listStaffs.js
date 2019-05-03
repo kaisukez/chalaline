0d4fda50d9e667c513f52509b3dbc4c8668e4121
kaisukez <zykigix@hotmail.com>
2019-05-03 15:56:03 +0700

    add user template

---
+++ users/listStaffs.js
@@ -0,0 +1,15 @@
+module.exports.listStaffs = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'list staffs is not finished, feel free to write some code',
+      // input: event,
+    }),
+  }
+
+  return response
+  // callback(null, response)
+}
