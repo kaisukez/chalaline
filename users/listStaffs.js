f969b07ebeb6034aefc39c0000e983a7e6e2b445
Natnaree Asavaseri <bunditnoikiku@hotmail.com>
2019-05-05 01:07:36 +0700

    fix foreach

--- users/listStaffs.js
+++ users/listStaffs.js
@@ -1,37 +1,46 @@
 const AWS = require('aws-sdk')
 AWS.config.update({
-    region: "ap-southeast-1",
+  region: "ap-southeast-1",
 });
-const cognito = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'})
+const cognito = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18'
+})
 const poolID = require('../pool-id.json')
 const userHelper = require('../helpers/user')
 
-async function getUsers (filter=undefined,filterAttributes=undefined) {
-    // filter example --> "atributeName = \"attributeValue\""
-    params = {
-        "Filter": filter,
-        "UserPoolId": poolID["UserPoolId"],
-        'AttributesToGet': filterAttributes
+async function getUsers(filter = undefined, filterAttributes = undefined) {
+  // filter example --> "atributeName = \"attributeValue\""
+  params = {
+    "Filter": filter,
+    "UserPoolId": poolID["UserPoolId"],
+    'AttributesToGet': filterAttributes
+  }
+  try {
+    const input = await cognito.listUsers(params).promise()
+    const awsUsers = input['Users']
+    var users = []
+    // console.log(awsUsers)
+    for (var i = 0; i < awsUsers.length; i++) {
+      var awsUser = awsUsers[i]
+      // console.log(awsUser,awsUsers[i])
+      var attributes = awsUser.Attributes
+      var user = userHelper.convertToDictUser(attributes)
+      user['username'] = awsUser.Username
+      users.push(user)
     }
-    try {
-        const rawUsers = await cognito.listUsers(params).promise()
-        var users = []
-        rawUsers['Users'].forEach(rawUser => {
-            var attributes = rawUser.Attributes
-            var user = userHelper.convertToDictUser(attributes)
-            users.push(user)
-        });
-        return users
-    } catch (err) {
-        console.log(err)
-    }
+    return users
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports.listStaffs = async (event, context, callback) => {
   var msg = ''
+
   try {
-    const raw_users = await getUsers()
-    var users = await userHelper.filterRoles(raw_users)
+    // const raw_users = await getUsers()
+    // var users = await userHelper.filterRoles(raw_users)
+    var users = await getUsers()
     msg = 'LIST STAFFS SUCCESSFULL'
   } catch (err) {
     msg = `SOME ERROR OCCUR ${err}`
@@ -43,7 +52,7 @@ module.exports.listStaffs = async (event, context, callback) => {
     },
     body: JSON.stringify({
       message: msg,
-      staffs:users
+      staffs: users
       // input: event,
     }),
   }
