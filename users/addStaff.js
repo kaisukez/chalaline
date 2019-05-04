00f162b4c96b82f809ffdac222e99567ae14d7e3
Natnaree Asavaseri <bunditnoikiku@hotmail.com>
2019-05-04 11:40:55 +0700

    add Staff

--- users/addStaff.js
+++ users/addStaff.js
@@ -1,72 +1,69 @@
 const AWS = require('aws-sdk')
 AWS.config.update({
-    region: "ap-southeast-1",
+  region: "ap-southeast-1",
 });
 const cognito = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: '2016-04-18'
+  apiVersion: '2016-04-18'
 })
 const poolID = require('../pool-id.json')
 
-async function addUser() {
+// input format 
+// {
+//   attributeName:attributeValue
+// }
+// email required
+
+
+function convertToAwsUser(user) {
+  var awsUser = []
+  for (attribute in user) {
+    awsUser.push({
+      'Name': attribute,
+      'Value': user[attribute]
+    })
+  }
+  return awsUser
+}
+
+async function addUser(user,temporaryPassword='1q2w3e4r') {
+  const awsUser = convertToAwsUser(user)
   var params = {
-      UserPoolId: poolID["UserPoolId"], /* required */
-      Username: 'testStaff', /* required */
-      TemporaryPassword: '1q2w3e4r',
-      UserAttributes: [
-          {
-              Name:'email',
-              Value: 'test-staff@gmail.com'
-          },
-          {
-              Name:'custom:custom:StoreID',
-              Value: "e7375563-bf7f-4923-951f-6502cc94f7e2"
-          },
-          {
-              Name:'custom:custom:branchID',
-              Value: "aaaaab"
-          },
-          {
-              Name:'custom:custom:role',
-              Value: "staff"
-          },
-      ],
-      ValidationData: [{
-              Name: 'email',
-              Value: 'test-staff@gmail.com'
-          },
-      ]
+    UserPoolId: poolID["UserPoolId"],
+    Username: user['email'].split('@')[0],
+    TemporaryPassword: temporaryPassword,
+    UserAttributes:awsUser,
+    ValidationData: convertToAwsUser({
+      'email': user['email']
+    })
   };
-  cognito.adminCreateUser(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
-    });      
+  await cognito.adminCreateUser(params, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {
+      console.log(data); // successful response
+      return data
+    }
+  });
+  return params
 }
 
 module.exports.addStaff = async (event, context, callback) => {
-  // const { email, StoreID,branchID,role, ...optional } = JSON.parse(event.body)
-
-  // console.log(event.body)
-  // UserAttributes
-  // const params = {
-  //   TableName,
-  //   Item: {
-  //     productID,
-  //     productName,
-  //     ...optional
-  //   }
-  // }
-
+  var msg = undefined
+  var user = JSON.parse(event.body)
+  user['custom:custom:role'] = 'staff'
+  try {
+    var params = await addUser(user)
+    msg = `ADD  ${user['email']} SUCCESSFULL`
+  } catch (err){
+    msg = `SOME ERROR OCCUR ${err}`
+  }
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify({
-      message: 'add staff is not finished, feel free to write some code',
-      input: event,
+      message: msg,
     }),
   }
-
   return response
-  // callback(null, response)
 }
